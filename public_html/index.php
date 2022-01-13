17db86c7206fc7de3bee6f5e472b40e4132583d4
tvainform <tvainform@yandex.ru>
2022-01-13 17:25:22 +0500

    Create project

---
+++ public_html/index.php
@@ -0,0 +1,6 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Тестовый сайт Создаватель");
+?>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
